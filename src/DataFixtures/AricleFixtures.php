7596b79eaf5731a19bfc30a32a15f1aadee64d7d
Khadim Niass <niassssn@gmail.com>
2020-08-07 12:33:09 +0000

    rajout du champ catégory dans le formulaire de creation de l'article,ajouter l'entity User,ajouter plus de text dans la page d'accueil,affichage des commentaires dans la page show

--- src/DataFixtures/AricleFixtures.php
+++ src/DataFixtures/AricleFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,17 +12,43 @@ class AricleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        for ($i=1; $i <=10 ; $i++) {
-            $article =new Article();
-            $article->setTitle("Titre de l'article n° .$i")
-                ->setContent("<p>Contenu de l'article n° .$i</p>")
-                ->setImage("//via.placeholder.com/350x150")
-                ->setCreateAt(new \DateTime());
+        $faker = \Faker\Factory::create('SN_sn');
+        for ($i = 0; $i < 3; $i++) {
+            $category=new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+            $manager->persist($category);
 
-            $manager->persist($article);
+            for ($j=1; $j <=mt_rand(4,6) ; $j++) {
+                $article =new Article();
+                $content ='<p>'. join($faker->paragraphs(5),'<p><p/>').'<p/>';
+
+                $article->setTitle($faker->sentence())
+                        ->setContent($content)
+                        ->setImage($faker->imageUrl())
+                        ->setCreateAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
+
+                $manager->persist($article);
+                //Donner des commentaires à l'article
+                for ($k=1; $k <=mt_rand(4,10) ; $k++){
+                    $comment=new Comment();
+                    $content ='<p>'. join($faker->paragraphs(2),'<p><p/>').'<p/>';
+                    $now=new \DateTime();
+                    $interval=$now->diff($article->getCreateAt());
+                    $days=$interval->days;
+                    $minimum='-'.$days.'days';
+
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreateAt($faker->dateTimeBetween($minimum))
+                            ->setArticle($article);
+                    $manager->persist($comment);
+                }
+            }
         }
+
         $manager->flush();
     }
 }
