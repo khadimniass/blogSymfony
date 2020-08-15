f350c02e559af89230792cfad69b7d43e27a4866
Khadim Niass <niassssn@gmail.com>
2020-08-15 19:35:19 +0000

    le bundle du message flash,stylisation de l'affichage,refactoring le code,styliser la page de login

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
 ];
